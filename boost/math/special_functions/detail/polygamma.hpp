c626b1817c53dba6598924bf73f822a5a0423e2c
Nikhar Agrawal <nikharagrawal2006@gmail.com>
2013-07-30 04:37:03 -0700

    add rudimentary skeleton for polygamma

---
+++ boost/math/special_functions/detail/polygamma.hpp
@@ -0,0 +1,41 @@
+
+///////////////////////////////////////////////////////////////////////////////
+//  Copyright 2013 Nikhar Agrawal
+//  Copyright 2013 Christopher Kormanyos
+//  Copyright 2013 John Maddock
+//  Copyright 2013 Paul Bristow
+//  Distributed under the Boost
+//  Software License, Version 1.0. (See accompanying file
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef _BOOST_BERNOULLI_B2N_2013_05_30_HPP_
+ #define _BOOST_BERNOULLI_B2N_2013_05_30_HPP_
+
+  #include <limits>
+  #include <vector>
+  #include <cmath>
+  #include <boost/cstdint.hpp>
+  #include <boost/math/special_functions/pow.hpp>
+  #include <boost/math/policies/policy.hpp>
+  #include <boost/static_assert.hpp>
+  #include <boost/mpl/if.hpp>
+  #include <boost/mpl/int.hpp>
+  #include <boost/type_traits/is_convertible.hpp>
+  #include <boost/multiprecision/cpp_dec_float.hpp>
+
+using namespace boost::multiprecision;
+using std::size_t;
+
+
+namespace boost { namespace math { namespace detail {
+
+  template<class T, class Policy>
+  inline T polygamma_imp(const int n, T x, const Policy &pol)
+  {
+        return x; //just a test return value
+  }
+
+
+} } } // namespace boost::math::detail
+
+#endif // _BOOST_BERNOULLI_B2N_2013_05_30_HPP_
---
+++ boost/math/special_functions/polygamma.hpp
@@ -0,0 +1,59 @@
+
+///////////////////////////////////////////////////////////////////////////////
+//  Copyright 2013 Nikhar Agrawal
+//  Copyright 2013 Christopher Kormanyos
+//  Copyright 2013 John Maddock
+//  Copyright 2013 Paul Bristow
+//  Distributed under the Boost
+//  Software License, Version 1.0. (See accompanying file
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef _BOOST_POLYGAMMA_2013_07_30_HPP_
+  #define _BOOST_POLYGAMMA_2013_07_30_HPP_
+
+  #include <boost/array.hpp>
+  #include <boost/cstdint.hpp>
+  #include "detail/polygamma.hpp"
+
+  namespace boost { namespace math {
+
+  template<class T, class Policy>
+  inline T polygamma(const int n, T x, const Policy &pol)
+  {
+        return boost::math::detail::polygamma_imp(n,x,pol);
+  }
+
+  template<class T>
+  inline T polygamma(const int n, T x)
+  {
+      return boost::math::polygamma(n,x,policies::policy<>());
+  }
+
+  template<class T, class Policy>
+  inline T digamma(T x, const Policy &pol)
+  {
+      return boost::math::polygamma(2,x,pol);
+  }
+
+  template<class T>
+  inline digamma(T x)
+  {
+      return boost::math::digamma(x,policies::policy<>());
+  }
+
+  template<class T, class Policy>
+  inline T trigamma(T x, const Policy &pol)
+  {
+      return boost::math::polygamma(3,x,pol);
+  }
+
+  template<class T>
+  inline trigamma(T x)
+  {
+      return boost::math::trigamma(x,policies::policy<>());
+  }
+
+
+} } // namespace boost::math
+
+#endif // _BOOST_BERNOULLI_2013_05_30_HPP_
