ab828d125ba8e51409342a2c24f8c0d9c6acab0d
Christopher Kormanyos <e_float@yahoo.com>
2013-09-08 15:34:02 +0200

    Added big-O checks for termination of Euler-Maclaurin summation and Taylor series. Slightly re-factored the code.

--- boost/math/special_functions/detail/polygamma.hpp
+++ boost/math/special_functions/detail/polygamma.hpp
@@ -8,40 +8,34 @@
 //  Software License, Version 1.0. (See accompanying file
 //  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-#ifndef _BOOST_POLYGAMMA_2013_07_30_CPP_
-  #define _BOOST_POLYGAMMA_2013_07_30_CPP_
+#ifndef _BOOST_POLYGAMMA_DETAIL_2013_07_30_HPP_
+  #define _BOOST_POLYGAMMA_DETAIL_2013_07_30_HPP_
 
-  #include <limits>
-  #include <vector>
   #include <cmath>
+  #include <limits>
   #include <boost/cstdint.hpp>
+  #include <boost/math/policies/policy.hpp>
+  #include <boost/math/special_functions/bernoulli.hpp>
   #include <boost/math/special_functions/trunc.hpp>
   #include <boost/math/special_functions/zeta.hpp>
-  #include <boost/math/policies/policy.hpp>
-  #include <boost/static_assert.hpp>
-  #include "../bernoulli.hpp"
   #include <boost/mpl/if.hpp>
   #include <boost/mpl/int.hpp>
+  #include <boost/static_assert.hpp>
   #include <boost/type_traits/is_convertible.hpp>
-  #include <boost/multiprecision/cpp_dec_float.hpp>
 
-using namespace boost::multiprecision;
-using std::size_t;
-
-
-namespace boost { namespace math { namespace detail {
+  namespace boost { namespace math { namespace detail {
 
   template<class T>
   struct max_iteration
   {
-     //TODO Derive a suitable formula based on precision of T
-     static const int value=2500;
+    //TODO Derive a suitable formula based on the precision of T
+    static const int value=2500;
   };
 
   template<class T>
   bool factorial_overflow(const int n)
   {
-    // Use Stirling's approximation to check if n! would overflow when data type is T
+    // Use Stirling's approximation to check if n! would overflow when data type is T.
     static const long int max_precision = std::numeric_limits<T>::max_exponent10;
 
     T nn                   = T(n);
@@ -51,9 +45,8 @@ namespace boost { namespace math { namespace detail {
     T base_10              = n_log_n_minus_n/log(10);
     long int base_10_ceil  = boost::math::ltrunc(base_10) + 1;
 
-    // since nlogn - n < log(n!) by a small margin, we add 10 as safety measure
-    //std::cout<<"base 10 exponent of "<<n<<"!: "<<base_10_ceil<<std::endl;
-    return (base_10_ceil + 10 > max_precision )? 1 : 0 ;
+    // Since nlogn - n < log(n!) by a small margin, we add 10 as safety measure.
+    return (((base_10_ceil + 10) > max_precision )? 1 : 0);
   }
 
   template<class T>
@@ -95,66 +88,64 @@ namespace boost { namespace math { namespace detail {
     unsigned int bernoulli_index = 100;
     boost::math::bernoulli_b2n<T>(bernoulli_index);
 
-    T z=x;
-    T log_z=T(log(z));
-    T one_over_2z= T(1)/(2*z);
-    T sum=T(0);
+    T z(x);
+    T log_z(log(z));
+    T one_over_2z= T(1) / (2 * z);
+    T sum(0);
 
-    for(int two_k=2; two_k < max_iteration<T>::value; two_k+=2)
+    for(int two_k = 2; two_k < max_iteration<T>::value; two_k += 2)
     {
       if(two_k/2 > static_cast<boost::int32_t>(bernoulli_index))
       {
-          try
-          {
-              int temp = static_cast<int>(bernoulli_index * 1.5F);
-              boost::math::bernoulli_b2n<T>(temp);
-              bernoulli_index = temp;
-          }
-          catch(...)
-          {
-              break;
-          }
+        try
+        {
+          int temp = static_cast<int>(bernoulli_index * 1.5F);
+          boost::math::bernoulli_b2n<T>(temp);
+          bernoulli_index = temp;
+        }
+        catch(...)
+        {
+          break;
+        }
       }
 
-      T term=T(1);
-      T one_over_two_k=T(1)/two_k;
-      T z_pow_two_k=pow(z,static_cast<boost::int32_t>(two_k));
-      T one_over_z_pow_two_k = T(1)/z_pow_two_k;
-      T bernoulli_term= boost::math::bernoulli_b2n<T>(two_k/2);
+      T term(1);
+      T one_over_two_k       = T(1) / two_k;
+      T z_pow_two_k          = pow(z, static_cast<boost::int32_t>(two_k));
+      T one_over_z_pow_two_k = T(1) / z_pow_two_k;
+      T bernoulli_term       = boost::math::bernoulli_b2n<T>(two_k / 2);
 
-      term = bernoulli_term * one_over_two_k * one_over_z_pow_two_k;
+      term = (bernoulli_term * one_over_two_k) * one_over_z_pow_two_k;
 
-      if(term == 0 )
+      if(term == 0)
       {
         continue;
       }
 
       sum += term;
 
-      T term_base_10_exp = term < 0 ? -term: term;
-      T sum_base_10_exp  = sum < 0 ? -sum: sum;
+      T term_base_10_exp = ((term < 0) ? -term : term);
+      T sum_base_10_exp  = ((sum  < 0) ? -sum  : sum);
 
       int ll;
 
-      T significand=frexp(term_base_10_exp,&ll);
-      term_base_10_exp=ll*0.303;
+      T significand = frexp(term_base_10_exp, &ll);
+      term_base_10_exp = ll * 0.303F;
 
-      significand=frexp(sum_base_10_exp,&ll);
-      sum_base_10_exp=ll*0.303;
+      significand = frexp(sum_base_10_exp, &ll);
+      sum_base_10_exp = ll * 0.303F;
 
-      long int order_check =  boost::math::ltrunc(term_base_10_exp)-boost::math::ltrunc(sum_base_10_exp);
+      long int order_check =  boost::math::ltrunc(term_base_10_exp) - boost::math::ltrunc(sum_base_10_exp);
       long int tol         =  std::numeric_limits<T>::digits10;
 
 
-      if((two_k > static_cast<boost::int32_t>(50)) && (order_check < -tol))
+      if((two_k > 24) && (order_check < -tol))
       {
-          break;
+        break;
       }
     }
 
-    T answer = log_z - one_over_2z -sum;
-
-    return answer;
+    return (log_z - one_over_2z) - sum;
   }
 
   template<class T, class Policy>
@@ -162,161 +153,171 @@ namespace boost { namespace math { namespace detail {
   {
      BOOST_MATH_STD_USING
 
-     if(n==0)
-        return digamma_atinfinityplus(n,x,pol);
+     if(n == 0)
+     {
+       return digamma_atinfinityplus(n, x, pol);
+    }
 
-//TODO try calculating for bernoulli_index= max_iteration, if error then set bernoulli_index=100
+     //TODO try calculating for bernoulli_index= max_iteration, if error then set bernoulli_index=100
      unsigned int bernoulli_index = 100;
      boost::math::bernoulli_b2n<T>(bernoulli_index);
 
-     const bool b_negate = (n % 2 == 0);
+     const bool b_negate = ((n % 2) == 0);
 
      const T n_minus_one_fact            = boost::math::factorial<T>(n - 1);
      const T nn                          = T(n);
      const T n_fact                      = n_minus_one_fact * nn;
-     const T one_over_z                  = T(1)/ x;
+     const T one_over_z                  = T(1) / x;
      const T one_over_z2                 = one_over_z * one_over_z;
-     const T one_over_z_pow_n            = T(1) / pow(x, static_cast<boost::int64_t>(n));
+     const T one_over_z_pow_n            = T(1) / pow(x, n);
            T one_over_x_pow_two_k_plus_n = one_over_z_pow_n * one_over_z2;
            T two_k_plus_n_minus_one      = nn + T(1);
-           T two_k_plus_n_minus_one_fact = n_fact * T(static_cast<boost::uint64_t>(n + 1)); //(n+3)! ?
-           T one_over_two_k_fact         = T(1)/2;
-           T sum                         = (  (boost::math::bernoulli_b2n<T>(static_cast<boost::uint64_t>(1u)) * two_k_plus_n_minus_one_fact)
-                                                  * (one_over_two_k_fact * one_over_x_pow_two_k_plus_n));
+           T two_k_plus_n_minus_one_fact = n_fact * (n + 1); //(n+3)! ?
+           T one_over_two_k_fact         = T(1) / 2;
+           T sum                         = (  (boost::math::bernoulli_b2n<T>(1) * two_k_plus_n_minus_one_fact)
+                                            * (one_over_two_k_fact * one_over_x_pow_two_k_plus_n));
 
      // Perform the Bernoulli series expansion.
-     for(boost::int32_t two_k = 4; two_k < max_iteration<T>::value; two_k += 2)
+     for(int two_k = 4; two_k < max_iteration<T>::value; two_k += 2)
      {
-        if((two_k / 2) > static_cast<boost::int32_t>(bernoulli_index))
+       if((two_k / 2) > static_cast<int>(bernoulli_index))
+       {
+         try
+         {
+           //TODO the multiplication factor should depend upon T, small precision, smaller multiplication factor
+           int temp = static_cast<int>(bernoulli_index * 2.0F);
+           boost::math::bernoulli_b2n<T>(temp);
+           bernoulli_index = temp;
+         }
+         catch(...)
+         {
+           break;
+         }
+       }
+
+       one_over_x_pow_two_k_plus_n *= one_over_z2;
+       two_k_plus_n_minus_one_fact *= ++two_k_plus_n_minus_one;
+       two_k_plus_n_minus_one_fact *= ++two_k_plus_n_minus_one;
+       one_over_two_k_fact         /= static_cast<boost::int32_t>(two_k * static_cast<boost::int32_t>(two_k - static_cast<boost::int32_t>(1)));
+
+       const T term = (  (boost::math::bernoulli_b2n<T>(two_k/2) * two_k_plus_n_minus_one_fact)
+                       * (one_over_two_k_fact * one_over_x_pow_two_k_plus_n));
+
+        if(term == 0)
         {
-           try
-           {
-              //TODO the multiplication factor should depend upon T, small precision, smaller multiplication factor
-              int temp = static_cast<int>(bernoulli_index * 2.0F);
-              boost::math::bernoulli_b2n<T>(temp);
-              bernoulli_index = temp;
-            }
-            catch(...)
-            {
-               break;
-            }
+          continue;
         }
 
-        one_over_x_pow_two_k_plus_n *= one_over_z2;
-        two_k_plus_n_minus_one_fact *= ++two_k_plus_n_minus_one;
-        two_k_plus_n_minus_one_fact *= ++two_k_plus_n_minus_one;
-        one_over_two_k_fact         /= static_cast<boost::int32_t>(two_k * static_cast<boost::int32_t>(two_k - static_cast<boost::int32_t>(1)));
+        sum += term;
 
-        const T term = (  (boost::math::bernoulli_b2n<T>(two_k/2) * two_k_plus_n_minus_one_fact)
-                              * (one_over_two_k_fact * one_over_x_pow_two_k_plus_n));
+        T term_base_10_exp = ((term < 0) ? -term : term);
+        T sum_base_10_exp  = ((sum  < 0) ? -sum  : sum);
 
-        if(term == 0 ) continue;
+        int ll;
 
-      sum += term;
+        T significand = frexp(term_base_10_exp, &ll);
+        term_base_10_exp = ll * 0.303F;
 
-      T term_base_10_exp = term < 0 ? -term: term;
-      T sum_base_10_exp  = sum < 0 ? -sum: sum;
+        significand = frexp(sum_base_10_exp, &ll);
+        sum_base_10_exp = ll * 0.303F;
 
-      int ll;
+        long int order_check =  boost::math::ltrunc(term_base_10_exp) - boost::math::ltrunc(sum_base_10_exp);
+        long int tol         =  std::numeric_limits<T>::digits10;
 
-      T significand=frexp(term_base_10_exp, &ll);
-      term_base_10_exp=ll * 0.303;
-
-      significand=frexp(sum_base_10_exp,&ll);
-      sum_base_10_exp = ll*0.303;
-
-      long int order_check =  boost::math::ltrunc(term_base_10_exp)-boost::math::ltrunc(sum_base_10_exp);
-      long int tol         =  std::numeric_limits<T>::digits10;
-
-
-        if((two_k > static_cast<boost::int32_t>(50)) && (order_check < -tol))
+        if((two_k > 24) && (order_check < -tol))
         {
           break;
         }
-
      }
 
      sum += ((((n_minus_one_fact * (nn + (x * static_cast<boost::int32_t>(2)))) * one_over_z_pow_n) * one_over_z) / 2);
 
-     return !b_negate ? sum : -sum;
+     return ((!b_negate) ? sum : -sum);
   }
 
   template<class T, class Policy>
   T polygamma_attransitionplus(const int n, const T &x, const Policy &pol)
   {
-  // this doesn't work for digamma either
+    // this doesn't work for digamma either
 
-      // Use Euler-Maclaurin summation.
+    // Use Euler-Maclaurin summation.
 
     // Use N = (0.4 * digits) + (4 * n)
     BOOST_MATH_STD_USING
-    static const boost::int64_t prec = static_cast<boost::int64_t>(std::numeric_limits<T>::digits10);
-    static const boost::int64_t d4d  = static_cast<boost::int64_t>(double(0.4) * prec);
-           const boost::int64_t N4dn = static_cast<boost::int64_t>(d4d + 4 * n);
-    static const boost::int64_t n32m = static_cast<boost::int64_t>(std::numeric_limits<boost::int32_t>::max());
-           const boost::int32_t N    = static_cast<boost::int32_t>(std::min(N4dn, n32m));
-           const boost::int32_t m    = n;
+    const int d4d  = static_cast<boost::int32_t>(0.4F * std::numeric_limits<T>::digits10);
+    const int N4dn = static_cast<boost::int32_t>(d4d + (4 * n));
+    const int N    = static_cast<boost::int32_t>((std::min)(N4dn, (std::numeric_limits<int>::max)()));
+    const int m    = n;
 
-    const boost::int64_t minus_m_minus_one = static_cast<boost::int64_t>(static_cast<boost::int32_t>(-m)
-                                                                        - static_cast<boost::int32_t>(1));
+    const int minus_m_minus_one = -m - 1;
 
-    T z                              = x;
-    T sum0                           = T(0);
-    T z_plus_k_pow_minus_m_minus_one = T(0);
+    T z(x);
+    T sum0(0);
+    T z_plus_k_pow_minus_m_minus_one(0);
 
-    for(boost::int32_t k = 1; k <= N; k++)
+    for(int k = 1; k <= N; ++k)
     {
       z_plus_k_pow_minus_m_minus_one = pow(z, minus_m_minus_one);
-
       sum0 += z_plus_k_pow_minus_m_minus_one;
-
       ++z;
     }
 
-    const T one_over_z_plus_N_pow_minus_m           = pow(z, static_cast<boost::int64_t>(-m));
+    const T one_over_z_plus_N_pow_minus_m           = pow(z, -m);
     const T one_over_z_plus_N_pow_minus_m_minus_one = one_over_z_plus_N_pow_minus_m / z;
 
-    const T term0 = one_over_z_plus_N_pow_minus_m_minus_one / static_cast<boost::int32_t>(2);
+    const T term0 = one_over_z_plus_N_pow_minus_m_minus_one / 2;
     const T term1 = one_over_z_plus_N_pow_minus_m           / m;
 
-          T sum1                                      = T(0);
-          T one_over_two_k_fact                       = T(1)/2;
-          boost::int32_t   mk                         = m + 1;
-          T am                                        = T(mk);
-    const T one_over_z_plus_N_squared                 = T(1) / (z * z);
-          T one_over_z_plus_N_pow_minus_m_minus_two_k = one_over_z_plus_N_pow_minus_m * one_over_z_plus_N_squared;
+          T   sum1                                      = T(0);
+          T   one_over_two_k_fact                       = T(1) / 2;
+          int mk                                        = m + 1;
+          T   am                                        = T(mk);
+    const T   one_over_z_plus_N_squared                 = T(1) / (z * z);
+          T   one_over_z_plus_N_pow_minus_m_minus_two_k = one_over_z_plus_N_pow_minus_m * one_over_z_plus_N_squared;
 
-    for(boost::int32_t k = 1; k < max_iteration<T>::value; k++)
+    for(int k = 1; k < max_iteration<T>::value; ++k)
     {
-      const boost::int32_t two_k = 2 * k; // k << 1
+      const int two_k = 2 * k; // k << 1
 
-      const T term = ((boost::math::bernoulli_b2n<T>(two_k/2) * am) * one_over_two_k_fact) * one_over_z_plus_N_pow_minus_m_minus_two_k;
+      const T term = ((boost::math::bernoulli_b2n<T>(two_k / 2) * am) * one_over_two_k_fact) * one_over_z_plus_N_pow_minus_m_minus_two_k;
 
-      /*const boost::int64_t order_check = term.order() - sum1.order();*/
+      T term_base_10_exp = ((term < 0) ? -term : term);
+      T sum_base_10_exp  = ((sum1 < 0) ? -sum1 : sum1);
 
-      //TODO Devise a good breaking condition
+      int ll;
 
-      if(k > 80 /*&& (order_check < -ef::tol())*/)
+      T significand = frexp(term_base_10_exp, &ll);
+      term_base_10_exp = ll * 0.303F;
+
+      significand = frexp(sum_base_10_exp, &ll);
+      sum_base_10_exp = ll * 0.303F;
+
+      long int order_check =  boost::math::ltrunc(term_base_10_exp) - boost::math::ltrunc(sum_base_10_exp);
+      long int tol         =  std::numeric_limits<T>::digits10;
+
+      if((two_k > 24) && (order_check < -tol))
       {
         break;
       }
 
       sum1 += term;
 
-      one_over_two_k_fact /= static_cast<boost::int32_t>(two_k + static_cast<boost::int32_t>(1));
-      one_over_two_k_fact /= static_cast<boost::int32_t>(two_k + static_cast<boost::int32_t>(2));
+      one_over_two_k_fact /= (two_k + 1);
+      one_over_two_k_fact /= (two_k + 2);
 
-      am *= static_cast<boost::int32_t>(++mk);
-      am *= static_cast<boost::int32_t>(++mk);
+      ++mk;
+      am *= mk;
+      ++mk;
+      am *= mk;
 
       one_over_z_plus_N_pow_minus_m_minus_two_k *= one_over_z_plus_N_squared;
     }
 
-    const T pg = (((sum0 + term0) + term1) + sum1) * factorial<T>(static_cast<boost::uint32_t>(m));
+    const T pg = (((sum0 + term0) + term1) + sum1) * factorial<T>(m);
 
-    const bool b_negate = (m % 2 == 0);
+    const bool b_negate = ((m % 2) == 0);
 
-    return (!b_negate ? pg : -pg);
+    return ((!b_negate) ? pg : -pg);
   }
 
   template<class T, class Policy>
@@ -341,9 +342,9 @@ namespace boost { namespace math { namespace detail {
           T k_plus_n_plus_one    =  T(n + 1);
     const T pg_kn                =  k_plus_n_fact * boost::math::zeta<T>(k_plus_n_plus_one);
           bool    b_neg_term     =  ((n % 2) == 0);
-          T sum                  =  !b_neg_term ? pg_kn : -pg_kn;
+          T sum                  =  ((!b_neg_term) ? pg_kn : -pg_kn);
 
-    const int break_value        =  possible_factorial_overflow_index<T>() - n -1;
+    const int break_value        =  possible_factorial_overflow_index<T>() - (n + 1);
 
     for(int k = 1; k < max_iteration<T>::value; k++)
     {
@@ -355,17 +356,28 @@ namespace boost { namespace math { namespace detail {
 
       const T term = (pg * z_pow_k) * one_over_k_fact;
 
-      //const boost::int64_t order_check = static_cast<boost::int64_t>(term.order() - sum.order());
+      T term_base_10_exp = ((term < 0) ? -term : term);
+      T sum_base_10_exp  = ((sum  < 0) ? -sum  : sum);
 
-      //TODO devise a good breaking condition
-      if(k > break_value /*&& (order_check < -ef::tol())*/)
+      int ll;
+
+      T significand = frexp(term_base_10_exp, &ll);
+      term_base_10_exp = ll * 0.303F;
+
+      significand = frexp(sum_base_10_exp, &ll);
+      sum_base_10_exp = ll * 0.303F;
+
+      long int order_check =  boost::math::ltrunc(term_base_10_exp) - boost::math::ltrunc(sum_base_10_exp);
+      long int tol         =  std::numeric_limits<T>::digits10;
+
+      if((k > 12) && (order_check < -tol))
       {
         break;
       }
 
       b_neg_term = !b_neg_term;
 
-      !b_neg_term ? sum += term : sum -= term;
+      ((!b_neg_term) ? sum += term : sum -= term);
     }
 
     return term0 + sum;
@@ -375,16 +387,20 @@ namespace boost { namespace math { namespace detail {
   template<class T, class Policy>
   inline T polygamma_imp(const int n, T x, const Policy &pol)
   {
-//    std::cout<<typeid(T).name()<<std::endl;
-        if(x < 0.5)
-            return polygamma_nearzero(n,x,pol);
-        else if (x > 400)
-            return polygamma_atinfinityplus(n,x,pol); //just a test return value
-        else
-            return polygamma_attransitionplus(n,x,pol);
+    if(x < 0.5F)
+    {
+      return polygamma_nearzero(n, x, pol);
+    }
+    else if (x > 400.0F)
+    {
+      return polygamma_atinfinityplus(n, x, pol); //just a test return value
+    }
+    else
+    {
+      return polygamma_attransitionplus(n, x, pol);
+    }
   }
-
 
 } } } // namespace boost::math::detail
 
-#endif // __BOOST_POLYGAMMA_2013_07_30_CPP_
+#endif // _BOOST_POLYGAMMA_DETAIL_2013_07_30_HPP_
